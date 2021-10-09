7025fd6e8d177a27397fe2515c081358e89212a3
genga <genga@students.cs.ubc.ca>
2021-10-09 13:17:39 -0700

    feat: more files added

---
+++ interview_prep_day_5/binary_search_tree.py
@@ -0,0 +1,50 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    def add_child(self, data):
+        if data == self.data:
+            return
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+    def in_order_traversal(self):
+        elements = []
+        # visit left tree first
+        if self.left:
+            elements += self.left.in_order_traversal()
+        # visit base node
+        elements.append(self.data)
+        if (self.right):
+            elements += self.left.in_order_traversal()
+
+        return elements
+    def build_tree(elements):
+        root = BinarySearchTreeNode(elements [0])
+        for i in range(1, len(elements)):
+            root.add_child(elements[i])
+        return root
+    def search(self, val):
+        if self.data == val:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else: 
+                return False
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else: 
+                return False
+
+
